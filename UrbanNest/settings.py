c814da79890bb7efb8369c8192e104889b90c4e7
Nikhil Gugwad <nikhilgugwad@gmail.com>
2024-11-06 18:03:49 +0530

    Install mysqlclient and create API app with initial configuration

--- UrbanNest/settings.py
+++ UrbanNest/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # Custom apps
-    'api',  
+    'api', 
 ]
 
 MIDDLEWARE = [
---
+++ api/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+
+# Placeholder for our views
+urlpatterns = []
